cac89f844be7dde9073a871f365f59a8fd079fc5
duongkinhhoang <82352379+duongkinhhoang@users.noreply.github.com>
2022-02-25 15:21:26 +0700

    Add files via upload

---
+++ Appium demo duong/src/test/java/pac2/Action.java
@@ -0,0 +1,15 @@
+package pac2;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
+
+import java.time.Duration;
+
+public class Action extends SetupStep {
+
+
+//    public void
+
+}
